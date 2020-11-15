2500ce971a33238d4fa1ed3be6c76c5bc6401bd8
alangogoro <cayne9160@gmail.com>
2020-11-15 14:49:10 +0800

    print in viewDidAppear

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let text = "1st Page"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(text)
     }
 
 }
