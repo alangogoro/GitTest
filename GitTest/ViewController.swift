88e4eae940485935dab2557bab87caeb73675d16
alangogoro <cayne9160@gmail.com>
2020-11-16 16:51:17 +0800

    Added customized button

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    
     let text = "1st Page"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        button.customized()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print("It is \(text)")
+        //print("It is \(text)")
     }
 
 }
